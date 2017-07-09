693c3184c3a879ee60a2fe994ebed987630b3af5
BehradMoeini <behrad.moeini9473@gmail.com>
2017-07-09 14:25:48 +0430

    added memory management algorithms

---
+++ src/memory/MFUMemoryManager.java
@@ -0,0 +1,14 @@
+package memory;
+
+public class MFUMemoryManager extends MemoryManager {
+
+	public MFUMemoryManager(int pageSize, int frameSize) {
+		super(pageSize, frameSize);
+	}
+
+	@Override
+	public void loadPage(int number) {
+		// TODO load page
+	}
+
+}
